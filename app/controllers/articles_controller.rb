44f488dbef902825416812f819a67a619c4274af
nalji95 <alexnalji@gmail.com>
2022-01-04 02:53:37 +0100

    create form but not saving in the db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,16 @@ class ArticlesController < ApplicationController
 
  def index
   @articles = Article.all
-  
+
  end
 
+ def new
+     
+ end
+
+ def create
+  # pour voir se qui se passe apres avoir cliqué sur save article,
+  #du moins pour voir les parametres prient en entrés
+  render plain: params[:article]
+ end
 end
