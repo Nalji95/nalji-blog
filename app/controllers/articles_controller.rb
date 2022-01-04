4183407f50520b9dd9ec03edd17c6b7a28a612c3
nalji95 <alexnalji@gmail.com>
2022-01-04 03:34:36 +0100

    create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,28 @@
 class ArticlesController < ApplicationController
 
- def show
-   # byebug
-  @article = Article.find(params[:id])
- end
+  def show
+    # byebug
+    @article = Article.find(params[:id])
+  end
 
- def index
-  @articles = Article.all
+  def index
+    @articles = Article.all
+  end
 
- end
+  def new
+    @article = Article.new
+  end
 
- def new
-     
- end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain:@article.inspect
+    if @article.save
+        flash[:notice]= "Article was successfully created"
+      redirect_to articles_path(@article)# without shortcut --> dirct to table 
+      #redirect_to @article ## with shortcut --> direct to index
+    else
+      render 'new'
+    end
+  end
 
- def create
-  # pour voir se qui se passe apres avoir cliqué sur save article,
-  #du moins pour voir les parametres prient en entrés
-  render plain: params[:article]
- end
 end
