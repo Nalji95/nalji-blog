140e45708fbf5640ad69a091caeadda9a7260b90
nalji95 <alexnalji@gmail.com>
2022-01-04 04:00:08 +0100

    update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    #byebug
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     #render plain:@article.inspect
@@ -22,6 +27,16 @@ class ArticlesController < ApplicationController
       #redirect_to @article ## with shortcut --> direct to index
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice]= "Article was successfully updated"
+      redirect_to articles_path(@article)# without shortcut
+    else
+      render 'edit'
     end
   end
 
